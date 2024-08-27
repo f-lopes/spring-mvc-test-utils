3e5c918da4fc09fe8f5bcbe74830fcf9ebeab5ac
Florian Lopes <florian.lopes@outlook.com>
2024-08-27 07:27:50 +0200

    feat: add support for Java 17 and Spring 6, restructure project
    - Add support for Java 17 and Spring Framework 6
    - Restructure project into a multi-module Maven project
    - Move smoke tests into their own module, `smoke-tests` for testing compatibility with different Java and Spring versions
    - Update README with compatibility notes

---
+++ spring-mvc-test-utils/src/test/java/io/florianlopes/spring/test/web/servlet/request/ConfigurationForm.java
@@ -0,0 +1,27 @@
+package io.florianlopes.spring.test.web.servlet.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Comparator;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConfigurationForm {
+
+    private static final Comparator<ConfigurationForm> COMPARATOR_NAME = Comparator.comparing(ConfigurationForm::getName);
+    private static String STATIC_NAME = "static name";
+    private final String finalName = "finalValue";
+    private String name;
+    private transient String transientName;
+    private Inner inner;
+
+    @Data
+    @AllArgsConstructor
+    public static class Inner {
+        private String value;
+    }
+
+}
