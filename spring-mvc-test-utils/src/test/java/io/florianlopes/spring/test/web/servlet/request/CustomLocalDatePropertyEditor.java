b184feb35c0d2feb4da1cf23816dffd26c9412b9
Florian LOPES <florian.lopes@outlook.com>
2024-08-28 09:40:42 +0200

    feat: add support for Java 17 and Spring 6, restructure project (#264)
    - Add support for Java 17 and Spring Framework 6
    - Restructure project into a multi-module Maven project
    - Move smoke tests into their own module, `smoke-tests` for testing compatibility with different Java versions
    - Update README with compatibility notes

---
+++ spring-mvc-test-utils/src/test/java/io/florianlopes/spring/test/web/servlet/request/CustomLocalDatePropertyEditor.java
@@ -0,0 +1,50 @@
+package io.florianlopes.spring.test.web.servlet.request;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Property editor for {@link java.time.LocalDate}
+ * Register this property editor with MockMvcRequestBuilderUtils#registerPropertyEditor
+ *
+ * @author Florian Lopes
+ * @see java.time.LocalDate
+ * @see java.time.format.DateTimeFormatter
+ * @see org.springframework.validation.DataBinder#registerCustomEditor
+ * @see io.florianlopes.spring.test.web.servlet.request.MockMvcRequestBuilderUtils#registerPropertyEditor(Class, PropertyEditor)
+ */
+class CustomLocalDatePropertyEditor extends PropertyEditorSupport {
+
+    private final DateTimeFormatter dateTimeFormatter;
+    private LocalDate localDate;
+
+    CustomLocalDatePropertyEditor(String dateFormatPattern) {
+        this.dateTimeFormatter = DateTimeFormatter.ofPattern(dateFormatPattern);
+    }
+
+    @Override
+    public Object getValue() {
+        return this.localDate;
+    }
+
+    @Override
+    public void setValue(Object value) {
+        this.localDate = (LocalDate) value;
+    }
+
+    @Override
+    public String getAsText() {
+        return this.localDate != null ? this.localDate.format(this.dateTimeFormatter) : "";
+    }
+
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        if (StringUtils.isNotEmpty(text)) {
+            this.localDate = LocalDate.parse(text, this.dateTimeFormatter);
+        }
+    }
+}
